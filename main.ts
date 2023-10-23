1863781fb59172880446c4d670b6f554e013701b
Sebastian Sanchez <js.sanchezs12@uniandes.edu.co>
2023-10-22 19:59:20 -0500

    Taller 2 completo

--- main.ts
+++ main.ts
@@ -5,27 +5,46 @@ const seriesTBody: HTMLElement = document.getElementById('series')!;
 
 const averageSeasonsT : HTMLElement = document.getElementById('seriesAve')!;
 
+
+
+function showSerieDetails(id: number): void {
+    let serie = series[id-1];
+    let serieDetailTBody: HTMLElement = document.getElementById('serieDetail')!;
+    serieDetailTBody.innerHTML = `<img class="card-img-top" src=${serie.image} referrerpolicy="no-referrer">
+                            <div class="card-body">
+                            <h2 class="card-body">${serie.name}</h2>
+                            <p class="card-text">${serie.description}</p>
+                            <a href="${serie.url}">${serie.url}</a>
+                            </div>`;
+}
+
+
 function renderSeriesInTable(series: Serie[]): void {
     series.forEach(c => {
         let trElement = document.createElement("tr");
         trElement.innerHTML = `<td>${c.id}</td>
-                               <td>${c.name}</td>
+                               <td>
+                               <a href="#">${c.name}</a>
+                               </td>
                                <td>${c.seasons}</td>
-                               <td>${c.channel}</td>`
+                               <td>${c.channel}</td>`;
+                               trElement.onclick = function() { showSerieDetails(c.id); };
         seriesTBody.appendChild(trElement);
     });
 }
+
 
 function averageSeasons(series: Serie[]): void {
     let sum= 0;
     series.forEach(c => {
         sum += c.seasons;
     });
-    let prom = sum / series.length;
+    let prom = sum / series.length; 
     let pElement = document.createElement("p");
     pElement.innerHTML = `<p>The average number of seasons is: ${prom}</p>`;
     averageSeasonsT.appendChild(pElement);
 }
+
 
 renderSeriesInTable(series);
 averageSeasons(series);
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,16 @@
 import { series } from "./data.js";
 var seriesTBody = document.getElementById('series');
 var averageSeasonsT = document.getElementById('seriesAve');
+function showSerieDetails(id) {
+    var serie = series[id - 1];
+    var serieDetailTBody = document.getElementById('serieDetail');
+    serieDetailTBody.innerHTML = "<img class=\"card-img-top\" src=".concat(serie.image, " referrerpolicy=\"no-referrer\">\n                            <div class=\"card-body\">\n                            <h2 class=\"card-body\">").concat(serie.name, "</h2>\n                            <p class=\"card-text\">").concat(serie.description, "</p>\n                            <a href=\"").concat(serie.url, "\">").concat(serie.url, "</a>\n                            </div>");
+}
 function renderSeriesInTable(series) {
     series.forEach(function (c) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                               <td>").concat(c.name, "</td>\n                               <td>").concat(c.seasons, "</td>\n                               <td>").concat(c.channel, "</td>");
+        trElement.innerHTML = "<td>".concat(c.id, "</td>\n                               <td>\n                               <a href=\"#\">").concat(c.name, "</a>\n                               </td>\n                               <td>").concat(c.seasons, "</td>\n                               <td>").concat(c.channel, "</td>");
+        trElement.onclick = function () { showSerieDetails(c.id); };
         seriesTBody.appendChild(trElement);
     });
 }
